9b71a4f454cbc29bc587d8963d6564ae9a09dbfe
carlosdamota <cdamota.cd@gmail.com>
2023-01-03 07:43:01 +0100

    correcion main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Persona perosna1 = new Persona();
+        perosna1.setEdad(30);
+        perosna1.setNombre("Carlos");
+        perosna1.setTelefono(666666666);
+
+        System.out.println("Edad: " + perosna1.getEdad());
+        System.out.println("Nombre: " + perosna1.getNombre());
+        System.out.println("Numero de telefono: " + perosna1.getTelefono());
     }
 }
+    class Persona{
+        private int edad;
+        private String nombre;
+        private int telefono;
+
+        public void setEdad (int edad){
+
+            this.edad = edad;
+        }
+
+        public int getEdad() {
+            return edad;
+        }
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setTelefono(int telefono) {
+            this.telefono = telefono;
+        }
+
+        public int getTelefono() {
+            return telefono;
+        }
+    }
+
